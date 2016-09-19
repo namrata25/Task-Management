2a75bb5eb6c736f0b104fdf67c8aa0c5fa2f1ff5
namrata25 <namratagupta188@gmail.com>
2016-09-19 22:19:41 +0530

    Initial commit

---
+++ client/TaskManagement.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import css from './styles/style.css';
+import Task from './component/Task';
+
+render(<Task /> , document.getElementById('root'));
---
+++ client/component/Task.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Task = React.createClass({
+  render() {
+    return(
+      <div id='header'>
+        Hello
+      </div>
+    )
+  }
+});
+
+export default Task;
